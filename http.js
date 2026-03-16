308bf10ecdbcf7f9461b9cb2f3e311ed7b2e4090
raheelnaziir <raheelnazirc22@gmail.com>
2026-03-16 20:16:56 +0500

    server completed

--- http.js
+++ http.js
@@ -6,7 +6,11 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
+    res.end("<h1><b>Raheel here</b></h1> <p>Hey there Raheel is here doing something something</p>");
 
 
+    if (req.url == "/about") {
+
+    }
 
 })
